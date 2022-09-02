df5a754b614639dec49bb966db5576b5db5ad57b
sthevanbello <sthevan.alves@gmail.com>
2022-09-02 20:43:51 -0300

    Classe CodeFirstContext

---
+++ Contexts/CodeFirstContext.cs
@@ -0,0 +1,18 @@
+﻿using CodeFirstApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApi.Contexts
+{
+    public class CodeFirstContext : DbContext
+    {
+        public CodeFirstContext(DbContextOptions<CodeFirstContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<Pedido> Pedido { get; set; }
+        public DbSet<ItemPedido> ItensPedido { get; set; }
+    }
+}
